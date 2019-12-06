bc682049bb9eb160b65782588e4c3ba074733b51
Moritz Rohde <moritz.rohde@tu-braunschweig.de>
2019-12-06 19:20:33 +0100

    Some adjustments in start.html and style

--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\index.html');
+router.get('/game', function(req, res, next) {
+    res.sendFile('/Users/moritz/WebstormProjects/bachelorarbeit/public/html/index.html');
+    //res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\index.html');
 });
 
 router.get('/start', function(req, res, next) {
-    res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\start.html');
+    //res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\start.html');
+    res.sendFile('/Users/moritz/WebstormProjects/bachelorarbeit/public/html/start.html');
+
 });
 module.exports = router;
 
