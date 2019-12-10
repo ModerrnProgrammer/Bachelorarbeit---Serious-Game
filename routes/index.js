ac7ff590fc9d82d69c60e8e75e2882325ecb66ab
Moritz Rohde <moritz.rohde@tu-braunschweig.de>
2019-12-10 17:47:27 +0100

    Update players in content and style

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
+
+router.get('/', function (req, res, next) {
+   res.redirect('/start');
+});
+
 router.get('/game', function(req, res, next) {
     res.sendFile('/Users/moritz/WebstormProjects/bachelorarbeit/public/html/index.html');
     //res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\index.html');
@@ -10,7 +15,11 @@ router.get('/game', function(req, res, next) {
 router.get('/start', function(req, res, next) {
     //res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\start.html');
     res.sendFile('/Users/moritz/WebstormProjects/bachelorarbeit/public/html/start.html');
+});
 
+router.get('/players', function(req, res, next) {
+    //res.sendFile('C:\\Users\\rohde\\WebstormProjects\\Games\\public\\html\\players.html');
+    res.sendFile('/Users/moritz/WebstormProjects/bachelorarbeit/public/html/players.html');
 });
 module.exports = router;
 
